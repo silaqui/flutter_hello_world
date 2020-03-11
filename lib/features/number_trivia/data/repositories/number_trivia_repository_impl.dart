6d09973326159d973b1e72e22ece69198c7cf75c
mjwa <mikolaj.wozniak@gft.com>
2020-03-11 14:58:22 +0100

    Contracts of Data Sources - dependency for repository

--- lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
+++ lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
@@ -1,9 +1,23 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutterhelloworld/core/error/failure.dart';
+import 'package:flutterhelloworld/core/platform/network_info.dart';
+import 'package:flutterhelloworld/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
+import 'package:flutterhelloworld/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
 import 'package:flutterhelloworld/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:flutterhelloworld/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'package:meta/meta.dart';
 
 class NumberTriviaRepositoryImpl implements NumberTriviaRepository {
+  final NumberTriviaLocalDataSource localDataSource;
+  final NumberTriviaRemoteDataSource remoteDataSource;
+  final NetworkInfo networkInfo;
+
+  NumberTriviaRepositoryImpl({
+    @required this.localDataSource,
+    @required this.remoteDataSource,
+    @required this.networkInfo,
+  });
+
   @override
   Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(int number) {
     // TODO: implement getConcreteNumberTrivia
@@ -15,5 +29,4 @@ class NumberTriviaRepositoryImpl implements NumberTriviaRepository {
     // TODO: implement getRandomNumberTrivia
     return null;
   }
-
 }
